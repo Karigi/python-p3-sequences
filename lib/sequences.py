511b03d90d430b8bffcb00f66cc1880d0983686e
Patty Karigi Kariuki <pattykarigi96@gmail.com>
2025-02-15 18:02:10 +0300

    Initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_series = list()
+    
+    for n in range(length):
+        if n == 0:
+            fibonacci_series.insert(n, n)
+        elif n == 1:
+            fibonacci_series.insert(n, n)
+        else:
+            fibonacci_series.insert(n, (fibonacci_series[n-2] + fibonacci_series[n-1]))
+
+    print(fibonacci_series) 
+    return fibonacci_series
